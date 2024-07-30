cdc864234f356a15982911fd63cda9a00359be68
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-07-30 16:50:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Travel App
+
+Installazione:
+
+- clonare repo del progetto
+- npm install per installare tutte le dipendenze
+- npm run dev per aprire finestra sul browser
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+import { store } from "../data/store";
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>CIao</h1>
+  <header>{{ store.counter }}</header>
+</template>
+
+<style lang="scss" scoped></style>
